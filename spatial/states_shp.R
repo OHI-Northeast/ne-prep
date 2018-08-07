584c5b8b45ba404e58798710489bc3552561b59c
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2018-08-06 17:07:44 -0700

    removed most of PA and NY NJ land

--- spatial/states_shp.R
+++ spatial/states_shp.R
@@ -1,20 +1,17 @@
 # Making a land shapefile
 
-#JA 12-30-2016
-
-source('~/github/ohi-northeast/src/R/common.R')
+source('~/github/ne-prep/src/R/common.R')
 
 #downloaded from US Census Bureau on 12-30-2016: https://www.census.gov/geo/maps-data/data/cbf/cbf_state.html
-states <- readOGR(dsn = file.path(dir_anx,'spatial/cb_2015_us_state_500k'),layer = 'cb_2015_us_state_500k')%>%
-  crop(wgs_ext)
+states <- st_read(dsn = file.path(dir_anx,'spatial/cb_2015_us_state_500k'),layer = 'cb_2015_us_state_500k')%>%
+  st_crop(wgs_ext) %>%
+  filter(NAME %in% c("Massachusetts", "New Hampshire", "Rhode Island", "Pennsylvania",
+                     "Connecticut", "New Jersey", "New York", "Maine", "Vermont")) %>%
+  dplyr::select(-STATEFP,-STATENS,-GEOID,-LSAD,-ALAND,-AWATER) %>%
+  st_crop(ne, xmin = -75, xmax = -66.9, ymin = 40.2, ymax = 47.5) %>%
+  st_transform("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0
++ellps=GRS80 +datum=NAD83 +units=m +no_defs +towgs84=0,0,0") #us_alb proj
 
-plot(states)
+plot(states[1])
 
-ne <- states[states$NAME %in% c("Massachusetts","New Hampshire","Rhode Island","Pennsylvania","Connecticut","New Jersey","New York","Maine","Vermont"),]
-
-ne@data <- ne@data%>%
-            dplyr::select(-STATEFP,-STATENS,-GEOID,-LSAD,-ALAND,-AWATER)
-
-out <- spTransform(ne,us_alb)
-
-writeOGR(out,dsn = 'spatial/shapefiles',layer = 'states',driver = 'ESRI Shapefile',overwrite_layer = T)
+st_write(states, dsn = 'spatial/shapefiles', layer = 'states.shp', driver = 'ESRI Shapefile', delete_layer = TRUE)
