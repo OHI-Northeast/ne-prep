7efe75ea644710f7dc403ede82594b9bc70dfe7f
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2018-11-19 10:30:18 -0800

    added text

--- spatial/rasterize_rgns.R
+++ spatial/rasterize_rgns.R
@@ -1,10 +1,11 @@
-## Creating a 0.5 degree raster for OHI-Northeast regions
+## Creating a 1km2 degree raster for OHI-Northeast regions
 
 source("~/github/ne-prep/src/R/common.R")
 
 #rasterize regions
 library(fasterize)
 
+#rasterizing each region to the same res as ocean_ne (1km2 cells)
 r <- fasterize(rgns, ocean_ne, field = "rgn_id")
 
 writeRaster(r, filename = "~/github/ne-prep/spatial/ocean_rasters/ne_rgns_rast.tif")
