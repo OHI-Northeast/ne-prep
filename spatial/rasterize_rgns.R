c0569c7cf3023b9d34fd1317fad6e305b29f7c79
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2019-03-25 10:56:22 -0700

    fixed zonal raster to be more recent regions shapefile

--- spatial/rasterize_rgns.R
+++ spatial/rasterize_rgns.R
@@ -8,4 +8,4 @@ library(fasterize)
 #rasterizing each region to the same res as ocean_ne (1km2 cells)
 r <- fasterize(rgns, ocean_ne, field = "rgn_id")
 
-writeRaster(r, filename = "~/github/ne-prep/spatial/ocean_rasters/ne_rgns_rast.tif")
+writeRaster(r, filename = "~/github/ne-prep/spatial/ocean_rasters/ne_rgns_rast.tif", overwrite = T)
