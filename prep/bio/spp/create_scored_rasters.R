d2f557bad5cb0e54cfbd544f7c427e65a8f21afd
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2019-06-11 11:18:56 -0700

    cleaning up spp after rerunning with new bird maps

--- prep/bio/spp/create_scored_rasters.R
+++ prep/bio/spp/create_scored_rasters.R
@@ -1,64 +1,45 @@
----
-title: "Create individual species conservation status rasters"
-author: "*Compiled on `r date()` by `r Sys.info()['user']`*"
-output: 
-  html_document:
-    code_folding: hide
-    toc: true
-    toc_depth: 3
-    toc_float: yes
-    number_sections: false
-    theme: cerulean
-    highlight: haddock
-    includes: 
-      in_header: '~/github/ne-prep/src/templates/ohi_hdr.html'
-  pdf_document:
-    toc: true
----
 
 # Summary
 
-This script creates scored rasters for each species depending on their conservation status and location. These rasters are primarily going to be used to create an aggregate species conservation risk map for the OHI Northeast region.
+# This script creates scored rasters for each species depending on their conservation status and location. These rasters are primarily going to be used to create an aggregate species conservation risk map for the OHI Northeast region.
 
 # Setup
 
-```{r setup, warning = F, message = F}
-knitr::opts_chunk$set(echo = TRUE, message = F, warning = F)
 source('~/github/ne-prep/src/R/common.R')
-```
+
 
 # Rasterize
 
 # Load data
 
-```{r cells}
+
 iucn_cells <- data.table::fread(file.path(dir_anx, "bio/iucn_sid_cells.csv")) %>%
   select(-V1)
 dp_cells   <- data.table::fread(file.path(dir_anx, "bio/ne_dataportal_spp_cells.csv")) %>%
   select(-V1) %>%
   mutate(common = tolower(str_replace_all(species, "_", " ")),
          common = str_replace(common, " normalized", "")) 
-```
 
-Load species rgn scores information
-```{r}
+
+#Load species rgn scores information
+
 spp_rgn_scores <- read_csv("~/github/ne-scores/region/layers/spp_status_scores.csv")
-```
 
 
-Read in rasters
-```{r}
+
+#Read in rasters
+
 portal_list <- list.files(file.path(dir_anx, "bio/portal_spp_rasters"), full.names = T)
 #iucn rasters
 iucn_rasters <- list.files(file.path(dir_anx, "bio/spp_presence_rasters"), full.names = T)
 
 #remove all rasters we don't want. I know this is ugly!
 iucn_list <- iucn_rasters[substr(iucn_rasters, 69, nchar(iucn_rasters)-4) %in% spp_rgn_scores$iucn_sid]
-```
 
-Function to score IUCN species maps
 
-```{r}
+#Function to score IUCN species maps
+
+
 cellid_rast <- raster("~/github/ne-prep/spatial/ocean_rasters/ne_cellids.tif")
 
 score_iucn_spp <- function(file){
@@ -75,11 +56,11 @@ score_iucn_spp <- function(file){
   writeRaster(status_raster, paste0(file.path(dir_anx), "/bio/scored_rasters/iucn_", sid, "_scored.tif"), overwrite = T)
   
 }
-```
 
-Function to score NE data portal species maps
 
-```{r}
+#Function to score NE data portal species maps
+
+
 score_dp_spp <- function(file){
   
   sp <- substr(file, 58, nchar(file)-4)
@@ -94,15 +75,37 @@ score_dp_spp <- function(file){
   writeRaster(status_raster, paste0(file.path(dir_anx), "/bio/scored_rasters/dp_", sp, "_scored.tif"), overwrite = T)
   
 }
-```
 
-Apply
+#Apply
 
-```{r, eval = F}
 lapply(iucn_list, score_iucn_spp)
 lapply(portal_list, score_dp_spp)
-```
 
 
+# create species risk map
+l <- list.files(file.path(dir_anx, "bio/scored_rasters"), full.names = T) %>% 
+  stack()
 
+species_risk_map <- sum(l, na.rm = T)
 
+## how many species per cell
+dp_count <- dp_cells %>%
+  group_by(cellID) %>%
+  count()
+
+iucn_count <- iucn_cells %>%
+  group_by(cellID) %>%
+  count()
+
+sp_per_cell <- iucn_count %>%
+  bind_rows(dp_count) %>%
+  group_by(cellID) %>%
+  summarize(num_species = sum(n))
+
+#rasterize 
+
+sp_per_cell_ras <- subs(cellid_rast, sp_per_cell, by = 1, which = 2)
+
+sp_risk_map <- species_risk_map/sp_per_cell_ras
+
+writeRaster(species_risk_map, filename = "~/github/ne-prep/prep/bio/spp/data/spp_status_risk.tif", overwrite = T)
